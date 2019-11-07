338a67e82c6d2d8cb408117f04c3b896c27d95b7
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2019-11-07 21:28:08 +0900

    だいたい動くバージョン

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # prosemirror-webpack-sample
-ProseMirror + Webpack sample
+
+## 概要
+
+* ProseMirror + Webpack sample
+
+## 実行環境
+
+* Node.js - 10.x
+* Yarn - 1.17.x
+
+## 使用ライブラリ
+
+* prosemirror-example-setup - 1.0.x
+* webpack - 4.41.x
+* webpack-cli - 3.3.x
+* webpack-dev-server - 3.9.x
+* copy-webpack-plugin - 5.0.x
+* css-loader - 3.2.x
+* mini-css-extract-plugin - 0.8.x
+
+## URL
+
+* https://prosemirror.net/examples/basic/
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+	entry: {
+    'editor': './src/editor.js',
+    'style': './src/style.css',
+  },
+	output: {
+    path: path.resolve(__dirname, 'dist'),
+		filename: '[name].bundle.js',
+	},
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: 8000
+  },
+  plugins: [
+    new CopyPlugin([
+      { from: 'src/index.html' },
+    ]),
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: { url: true }
+          }
+        ]
+      }
+    ]
+  }
+}
